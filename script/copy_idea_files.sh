9a2c14899e1d803c307db299c9c0403e803f1925
Hågen Hasle <haagenhasle@gmail.com>
2023-01-20 22:03:17 +0100

    Fjernet unødvendige linjer, meta exec kjøres også i hovedprosjektet

--- script/copy_idea_files.sh
+++ script/copy_idea_files.sh
@@ -6,5 +6,3 @@ meta exec 'echo "!.idea/codeStyles/codeStyleConfig.xml" >> .gitignore'
 meta exec 'echo "!.idea/codeStyles/Project.xml" >> .gitignore'
 cp template-data/Project.xml .idea/codeStyles/
 cp template-data/codeStyleConfig.xml .idea/codeStyles/
-echo "!.idea/codeStyles/codeStyleConfig.xml" >> .gitignore
-echo "!.idea/codeStyles/Project.xml" >> .gitignore

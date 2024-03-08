d0fb43b90dc8e8d1a2b7dd9bd688fdad2167e0f7
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2024-03-08 11:15:32 +0100

    ... glemte en add

--- dkill.sh
+++ dkill.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+docker_cmd="docker compose down"
+if command -v docker-compose &> /dev/null
+then
+    echo "Bruker docker-compose"
+    docker_cmd="docker-compose down"
+fi
+$docker_cmd
 docker ps -aq | xargs docker stop | xargs docker rm
 docker network ls | grep tiltakspenger | sed 's/ .*//' | xargs docker network rm

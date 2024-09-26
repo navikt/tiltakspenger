5c3d7b1171705829cad034b41b65d94beffb3b89
John Andre Hestad <john.andre.hestad@nav.no>
2024-09-26 16:19:46 +0200

    Fiks well known lokalt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 repositories {
     // Dette hjalp John Andre Hestad å laste ned sources/documentation i IntelliJ.
     mavenCentral()
+    maven("https://packages.confluent.io/maven/")
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    maven("https://plugins.gradle.org/m2/")
+    maven("https://build.shibboleth.net/maven/releases/")
 }

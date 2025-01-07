be3bf81b3c9a1d977e7560891f862268d1c36c1e
John Andre Hestad <john.andre.hestad@nav.no>
2025-01-07 13:20:57 +0100

    Fjern template-data

--- script/copy_idea_files.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-meta exec "cp ../template-data/Project.xml .idea/codeStyles/"
-meta exec "cp ../template-data/codeStyleConfig.xml .idea/codeStyles/"
-meta exec 'echo "!.idea/codeStyles/codeStyleConfig.xml" >> .gitignore'
-meta exec 'echo "!.idea/codeStyles/Project.xml" >> .gitignore'
-cp template-data/Project.xml .idea/codeStyles/
-cp template-data/codeStyleConfig.xml .idea/codeStyles/
